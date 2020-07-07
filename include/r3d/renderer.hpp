4537480f56758e5182a03a13abc2bae1f3716a67
Alexandru Ica <alexandruica703@gmail.com>
2020-07-07 11:29:03 +0300

    Implement a simple function to test that the project builds correctly

---
+++ include/r3d/renderer.hpp
@@ -0,0 +1,11 @@
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+#pragma once
+
+namespace r3d {
+
+auto test() -> void;
+
+}
+
+#endif // !RENDERER_HPP
---
+++ src/renderer.cpp
@@ -0,0 +1,12 @@
+#include <spdlog/spdlog.h>
+
+#include "r3d/renderer.hpp"
+
+namespace r3d {
+
+auto test() -> void
+{
+    spdlog::info("Hello!");
+}
+
+} // namespace r3d
