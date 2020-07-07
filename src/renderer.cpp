2e87273b99068902643bcc7dfebc3d58f7f8f0de
Alexandru Ica <alexandruica703@gmail.com>
2020-07-07 12:07:11 +0300

    Adding SDL2 as a dependency and making sure it works

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,11 +1,15 @@
-#include "r3d/renderer.hpp"
+#include <SDL.h>
+
 #include "r3d/log.hpp"
+#include "r3d/renderer.hpp"
 
 namespace r3d {
 
 auto test() -> void
 {
     LOG("Hello {}!", "there");
+
+    SDL_Init(SDL_INIT_VIDEO);
 }
 
 } // namespace r3d
