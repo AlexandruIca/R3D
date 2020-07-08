86148849aa5481c06694c88a6057caa3e939e7ad
Alexandru Ica <alexandruica703@gmail.com>
2020-07-08 13:31:11 +0300

    Provide macro for `spdlog::error`

--- include/r3d/log.hpp
+++ include/r3d/log.hpp
@@ -5,9 +5,15 @@
 #include <spdlog/spdlog.h>
 
 #ifdef R3D_DEBUG
+
 #define LOG(...) spdlog::info(__VA_ARGS__)
+#define ERROR(...) spdlog::error(__VA_ARGS__)
+
 #elif defined(R3D_RELEASE)
+
 #define LOG(...) static_cast<void>(0)
+#define ERROR(...) static_cast<void>(0)
+
 #endif
 
 #endif // !R3D_LOG_HPP
